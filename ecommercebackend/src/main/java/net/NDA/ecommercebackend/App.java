b08b5d09a009eab87b7f4bd7444aff7a8f2d40b9
Adjani <nassiadjani@yahoo.com>
2018-02-17 09:01:52 +0100

    My project completed successfully

---
+++ ecommercebackend/src/main/java/net/NDA/ecommercebackend/App.java
@@ -0,0 +1,13 @@
+package net.NDA.ecommercebackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
